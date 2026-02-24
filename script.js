8c48d556f74e0ca97f800e6921ab0edd17634776
saremecin-hue <saremecin@gmail.com>
2026-02-24 21:26:05 +0300

    Add goTo function for page navigation

---
+++ script.js
@@ -0,0 +1,7 @@
+function goTo(pageId) {
+  document.querySelectorAll(".page").forEach(page => {
+    page.classList.remove("active");
+  });
+
+  document.getElementById(pageId).classList.add("active");
+}
